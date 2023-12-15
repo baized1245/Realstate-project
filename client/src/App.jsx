33b792d83755ecfcde9bebf70379b8064fe35497
unknown <you@example.com>
2023-12-15 17:50:48 +0600

    Complete create listing page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import CreatListing from "./pages/CreatListing";
 
 export default function App() {
   return <BrowserRouter>
@@ -18,6 +19,7 @@ export default function App() {
       <Route path="/about" element={<About/>} />
       <Route element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile/>} />
+      <Route path="/create-listing" element={<CreatListing/>} />
       </Route>
     </Routes>
    </BrowserRouter>;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from './../firebase';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteStart, deleteUserSuccess, deleteFailure, signOutUserStart, signOutUserSuccess, signOutUserFailure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import {Link} from 'react-router-dom'
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -125,6 +126,7 @@ export default function Profile() {
         <input onChange={handleChange} type="email" placeholder='email' id='email' defaultValue={currentUser.email} className='border p-3 rounded-lg' />
         <input onChange={handleChange} type="password" placeholder='password' id='password' className='border p-3 rounded-lg' />
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-80' to={'/create-listing'}>Creat Listing</Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
