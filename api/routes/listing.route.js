3416c9205189967d00840ea2fdeebae72d7fd3d9
unknown <you@example.com>
2023-12-17 20:35:20 +0600

    Complete create listing page functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,6 +5,6 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 
-router.post('/creat', verifyToken, creatListing);
+router.post('/create', verifyToken, creatListing);
 
 export default router;
